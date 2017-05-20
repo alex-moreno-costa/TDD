9109d5b4994c59c9f6c766efa9554040cacb1756
Alex Moreno <alex.moreno.costa@gmail.com>
2017-05-20 19:13:23 -0300

    Feito o teste de maior e menor

---
+++ tests/Loja/Produto/MaiorEMenorTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CDC\Loja\Produto;
+
+use CDC\Loja\Carrinho\CarrinhoDeCompras;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class MaiorEMenorTest extends PHPUnit
+{
+    public function testOrdemDecrescente() {
+
+        $carrinho = new CarrinhoDeCompras();
+
+        $carrinho->adiciona(new Produto("Geladeira", 450.00));
+        $carrinho->adiciona(new Produto("Liquidificador", 250.00));
+        $carrinho->adiciona(new Produto("Jogo de pratos", 70.00));
+
+        $maiorMenor = new MaiorEMenor();
+        $maiorMenor->encontra($carrinho);
+
+        $this->assertEquals("Jogo de pratos", $maiorMenor->getMenor()->getNome());
+        $this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+    }
+}
