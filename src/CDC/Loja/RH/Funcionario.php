2789a8fd4d1efc5d6d7f5169891ada2d26526c4a
Alex Moreno <alex.moreno.costa@gmail.com>
2017-05-21 13:53:03 -0300

    finalizado capitulo 4

---
+++ src/CDC/Loja/RH/Funcionario.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+
+class Funcionario
+{
+    protected $nome;
+    protected $salario;
+    protected $cargo;
+
+    public function __construct($nome,$salario,$cargo)
+    {
+        $this->nome = $nome;
+        $this->cargo = $cargo;
+        $this->salario = $salario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSalario()
+    {
+        return $this->salario;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+}
---
+++ tests/Loja/RH/CalculadoraDeSalarioTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class CalculadoraDeSalarioTest extends PHPUnit
+{
+    public function testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+
+        $desenvolvedor = new Funcionario('André',1500.0, TabelaCargos::DESENVOLVEDOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(1500*0.9,$salario,null,0.00001);
+    }
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAcimaDoLimite()
+    {
+        $calculadora = new CalculadoraDeSalario();
+
+        $desenvolvedor = new Funcionario('André',4000.0, TabelaCargos::DESENVOLVEDOR);
+
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+
+        $this->assertEquals(4000*0.8,$salario,null,0.00001);
+    }
+
+//    public function testCalculoSalarioParaDBAsComSalarioAbaixoDoLimite()
+//    {
+//        $calculadora = new CalculadoraDeSalario();
+//
+//        $dba = new Funcionario('André',500.0, TabelaCargos::DBA);
+//
+//        $salario = $calculadora->calculaSalario($dba);
+//
+//        $this->assertEquals(500*0.85,$salario,null,0.00001);
+//    }
+}
